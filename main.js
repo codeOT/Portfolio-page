8d973222e9107c04ce8f511d3cac3080a77b1a84
Oni Toheeb Damilare <toheeboni@gmail.com>
2022-10-14 10:04:17 +0100

    contact now working

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-function sendMend() {
-    const params = {
-        from_name: document.getElementById('fullName'),
-        email_id: document.getElementById('email_id'),
-        message: document.getElementById('message')
+function SendMail() {
+    let params = {
+        from_name: document.getElementById('fullName').value,
+        email_id: document.getElementById('email_id').value,
+        message: document.getElementById('message').value
     }
     emailjs.send('service_vg2ry7i', 'template_jelgfz9', params).then(function(res) {
         alert('success ' + res.status)
+        
     })
+    console.log('worked')
 }
