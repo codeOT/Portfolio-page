e63ece4bc60c24e8d909e325afd262f1759fa9ef
Oni Toheeb Damilare <toheeboni@gmail.com>
2022-10-28 11:06:44 +0100

    typing

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
-// function SendMail() {
-//     let params = {
-//         from_name: document.getElementById('fullName').value,
-//         email_id: document.getElementById('email_id').value,
-//         message: document.getElementById('message').value
-//     }
-//     emailjs.send('service_vg2ry7i', 'template_jelgfz9', params).then(function(res) {
-//         alert('success ' + res.status)
-        
-//     })
-//     console.log('worked')
-// }
+let typed = new Typed(".auto-type", {
+    strings: ['Front-End Developer', 'Technical Writer', 'CodeOT'],
+    typeSpeed: 70,
+    backSpeed: 80,
+    loop: true
+
+})
